9696f33524179cf402785726f6dd3ba4699d37bb
Jacob Marthaller <https://github.com/jmarthaller>
2021-03-23 06:15:26 -0700

    add value to filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -10,11 +10,11 @@ function Filter( { currentUser, handleCategoryChange, selectedCategory } ) {
             
           <select name="filter" id="filter" onChange={handleCategoryChange}>
             <option value=''>Filter by Cuisine</option>
-            <option value="Korean">Korean</option>
-            <option value="Healthy">Healthy</option>
+            <option value="European">European</option>
+            <option value="Tex Mex">Healthy</option>
             <option value="Greek">Greek</option>
             <option value="American (New)">American (New)</option>
-            <option value="Argentinian">Argentinian</option>
+            <option value="American (Traditional)">American (Traditional)</option>
             <option value="Vegetarian">Vegetarian</option>
             <option value="Caribbean">Caribbean</option>
           </select>
