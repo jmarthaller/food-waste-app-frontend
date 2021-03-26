b7a7acc32ff6780f46a5960196461bdac0387cac
Jacob Marthaller <marthaller.jr@gmail.com>
2021-03-26 07:19:00 -0700

    delete comments from FavoritesTile.js

--- src/components/FavoritesTile.js
+++ src/components/FavoritesTile.js
@@ -2,9 +2,6 @@ import React from 'react';
 
 function FavoritesTile({ id, name, image, cuisine, subtractFavorite }) {
     
-    // function handleDislike() {
-    //     subtractFavorite(id)
-    // }
 
 
     return (
@@ -13,7 +10,6 @@ function FavoritesTile({ id, name, image, cuisine, subtractFavorite }) {
       <img src={image} alt={name} className="food-img"/>
       <p>{cuisine}</p>
       
-      {/* <button onClick={handleDislike}>Remove from Favorites</button> */}
     </div>
     )
 }
